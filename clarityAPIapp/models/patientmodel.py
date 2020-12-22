f3384e6e4209e709a6c08514cb2f235809aa9d32
Kirk Sudduth <kirksudduth@gmail.com>
2020-12-22 15:41:39 -0600

    modified patient model

--- clarityAPIapp/models/patientmodel.py
+++ clarityAPIapp/models/patientmodel.py
@@ -40,6 +40,47 @@ class PatientModel(models.Model):
     child_last_name = models.CharField(max_length=25)
     child_gender = models.CharField(max_length=25)
     child_dob = models.DateField(auto_now_add=False)
+    marital_status = models.CharField(max_length=25)
+    spouse_first_name = models.Charfield(max_length=25)
+    spouse_last_name = models.Charfield(max_length=25)
+    spouse_gender = models.CharField(max_length=25)
+    spouse_dob = models.DateField(auto_now_add=False)
+    consent = models.BooleanField(default=False)
+
+    # Termination of Parental Rights
+    unforeseeable_future_change = models.BooleanField(default=False)
+    conviction_imprisonment_felony = models.BooleanField(default=False)
+    emotional_illness_mental_deficiency = models.BooleanField(default=False)
+    unable_discharge_childcare_responsibility = models.BooleanField(
+        default=False)
+    excess_use_controlled_substance = models.BooleanField(default=False)
+    rehab_failure = models.BooleanField(default=False)
+    serious_bodily_injury_child = models.BooleanField(default=False)
+    willful_neglect_abandonment = models.BooleanField(default=False)
+    loss_custody_other_children = models.BooleanField(default=False)
+    failure_maintain_material_needs = models.BooleanField(default=False)
+    failure_maintain_consistent_contact = models.BooleanField(default=False)
+    parent_has_abused_child = models.BooleanField(default=False)
+    lack_effort_adjust_circumstances = models.BooleanField(default=False)
+
+    # Interview
+    times_DHR_interacted_with = models.CharField(max_length=25)
+    inference_observations = models.CharField(max_length=300)
+    dhr_alleged = models.CharField(max_length=500)
+    misconstrued_dhr = models.CharField(max_length=400)
+    truth_in_dhr_allegations = models.CharField(max_length=500)
+    dhr_steps_taken = models.CharField(max_length=300)
+    dhr_steps_taken_by_patient = models.Charfield(max_length=400)
+    understanding_why_here = models.CharField(max_length=400)
+
+    times_involved_dhr_al = models.CharField(max_length=25)
+    times_involved_other_child_protective_agencies = models.CharField(
+        max_length=300)
+    how_good_emotionally_now_and_future_children = models.CharField(
+        max_length=500)
+    how_temperament_dovetail_with_children_now_future = models.CharField(
+        max_length=500)
+    how_discipline_child = models.CharField(max_length=200)
 
     class Meta:
         verbose_name = ("patient")

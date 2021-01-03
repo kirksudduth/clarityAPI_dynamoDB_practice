e943ea1b9169e44ba4ff350aa5f7d941304c1a1e
Kirk Sudduth <kirksudduth@gmail.com>
2021-01-02 23:24:30 -0600

    added keys to the add_user function in API_calls.py

--- clarityAPIapp/APImanager/API_calls.py
+++ clarityAPIapp/APImanager/API_calls.py
@@ -5,12 +5,23 @@ dynamodb = boto3.resource('dynamodb')
 
 table = dynamodb.Table('clarityAPI')
 
-table.put_item(
-    Item={
-        'First_Name': 'Tanya',
-        'Location': 'Dayton',
-        'PK': 'EMPLOYEE#'+str(uuid.uuid4()),
-        'SK': 'LOCATION#2'
-    }
-)
-print("TABLE: ", table)
+
+def add_user(request):
+    if request.method == 'POST':
+        # put if conditional where check value of is_staff
+        table.put_item(
+            Item={
+                'first_name': 'Tanya',
+                'middle_name': 'Dayton',
+                'last_name': 'Dayton',
+                'email': 'email',
+                'username': 'username',
+                'password': 'password',
+                'is_staff': True,
+                'title': 'Important Person',
+                'is_admin': True,
+                'PK': 'EMPLOYEE#'+str(uuid.uuid4()),
+                'SK': 'LOCATION#2'
+            }
+        )
+        print("TABLE: ", table)
